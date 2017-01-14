61328dfa8d51e427fd0da72964f4e9576c9f7017
Peter J. Truong <hp5340@gmail.com>
2017-01-13 18:34:08 -0800

    completed 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+attr_accessor :title
+	def title
+		if @title.include? " "
+			@title = @title.split.map do |word| 
+				if word == "and" || word == "in" || word == "the" || word == "of" || word =="a" || word =="an"
+					word 
+				else
+					word.capitalize
+				end
+			end
+
+			@title[0].capitalize!	
+			@title.join(" ")
+		else	
+		@title.capitalize
+		end
+	end
+
 end
