374eb71d6acfb1105a3d8abfb7db5089fddf6b66
Peter J. Truong <hp5340@gmail.com>
2017-01-13 19:18:29 -0800

    completed 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+ 
+  def initialize
+  	@seconds = 0
+  end
+
+  
+	def time_string 
+		hours = @seconds / 3600
+		minutes = (@seconds % 3600) / 60
+		seconds = (@seconds % (3600)) % 60
+
+	add_zero(hours) + ":" + add_zero(minutes) + ":" + add_zero(seconds)
+  	
+  end
+
+  def add_zero(num)
+  	if num < 10
+  		"0" + num.to_s
+  	else
+  		num.to_s
+  	end
+  end
+
 end
