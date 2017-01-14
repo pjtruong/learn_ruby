8e3999a76525d9b25f2cf44b8efd0ce8eb334885
Peter J. Truong <hp5340@gmail.com>
2017-01-13 18:00:03 -0800

    completed 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,34 @@
 #write your code here
+def translate (phrase)
+	phrase = phrase.split.map do |piece|
+
+		check(piece)
+
+	end
+
+	phrase.join(" ")
+	
+end
+
+def check (word)
+	vowels = ["a","e","i","o","u"]
+
+	if vowels.include? word[0]
+		word = word + "ay"
+	else 
+		i = 0
+
+		while !(vowels.include? word[0])
+			if word[0] == "q" && word[1] =="u"
+				word = word[2..word.length] + word[0..1] 
+			else 
+				word = word[1..word.length] + word[0]
+			end
+			
+			
+		end
+
+		word + "ay"
+	end
+
+end
