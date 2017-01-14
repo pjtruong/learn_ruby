25210259cb5b24161202ce9872f19efbed573b66
Peter J. Truong <hp5340@gmail.com>
2017-01-13 16:25:37 -0800

    completed 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+def add(num1,num2)
+	num1 + num2
+end
+
+def subtract(num1,num2)
+	num1 - num2
+end
+
+def sum(array)
+	if array.any?
+	
+	array.inject{|sum,x| sum + x}
+		
+	
+
+	else 
+		total = 0
+
+	end	
+
+end
+
+def multiply(*nums)
+	nums.inject(:*)
+end
+
+
+def power(num1,num2)
+	num1**num2
+end
+
+
+def factorial(n)
+	if n == 0
+		1
+	else
+		n * factorial(n-1)
+	end
+end
+
+		
